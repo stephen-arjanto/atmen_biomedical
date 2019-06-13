6d07a6c212f6c13c65a14d31827b628afd276794
stephen-arjanto <stephen.arjanto@gmail.com>
2019-06-12 21:59:28 -0400

    Add undersampling method

--- data_processing.py
+++ data_processing.py
@@ -7,71 +7,50 @@ import statistics
 import pandas as pd
 import numpy as np
 
+import datetime
+
 # Simulate data reading
 df = pd.read_excel('/Users/stephenarjanto/Desktop/cloud/trial4.xlsx', sheet_name='Sheet1')
 df.columns = ['Value']
 
+# Obtain zero input average values and noise standard deviation
 zero_avg = np.mean(df.iloc[0:49,0])
 noise = statistics.stdev(df.iloc[0:49,0])
+
+# Determine signal cutoff & remove unneeded data points
 threshold_noise = 4*noise
-# print('noise_stdev = ', noise)
-# print('zero_avg = ', zero_avg)
-
 threshold_signal = zero_avg - threshold_noise
-# print('threshold_signal = ', threshold_signal)
-
-# Remove all values larger than threshold signal
 clean_df = df[df['Value']<threshold_signal] - zero_avg
 
-# print('df', df)
-# print('clean_df', clean_df)
-
-# Need to divide the clean_df into two parts (inhale and exhale)
-# print(clean_df.index.values)
-# first_index = clean_df.index.values[0]
-# print('first_index = ', first_index)
-
-# cutoff_index = 0
-# for entry in clean_df.index.value:
-# 	current_entry = entry
-
-# 	previous_entry = current_entry
-
+# Obtain signal index
 index_list = clean_df.index.values
-# print(index_list)
 
 # Calculate the difference between values in the index list 
 dif_index = [index_list[i+1] - index_list[i] for i in range(len(index_list)-1)]
 
-# Threshold index
+# Threshold index (minimum "jump" required)
 threshold_index = 50
 
-# print('dif_index = ', dif_index)
-
-# Find the index where the index list "jump"
+# Find the index where the index list "jump" and separate data
 separation_index = next(x for x, val in enumerate(dif_index) if val > threshold_index)
-# print('separation_index = ' , separation_index)
-
 inhalation = clean_df.iloc[0:separation_index+1,:]
 exhalation = clean_df.iloc[separation_index+1:-1,:]
+# print('separation_index = ' , separation_index)
 
-# print('inhalation = ', inhalation)
-# print('exhalation = ', exhalation)
+# Remove built-in index
+new_inhalation = inhalation.values[:,0]
+new_exhalation = exhalation.values[:,0]
 
-# Reset the index
-# new_inhalation = inhalation.reset_index(drop=True)
-# new_exhalation = exhalation.reset_index(drop=True)
-new_inhalation = inhalation.values
-new_exhalation = exhalation.values
-
+# Interpolate between inhalation data points
 x_inhale = np.linspace(1,len(new_inhalation),len(new_inhalation))
-print('x_inhale = ', x_inhale)
-new_inhalation = new_inhalation[:,0]
-print('new_inhale = ', new_inhalation)
-
-evaluation_points = np.linspace(0,70,25)
-evaluation_points = evaluation_points
+evaluation_points = np.linspace(0,len(new_inhalation),25)
 final_inhale = np.interp(evaluation_points,x_inhale,new_inhalation)
 
-print('final_inhale = ', final_inhale)
-print('final_inhale length =', len(final_inhale))
+# Interpolate between exhalation data points
+x_exhale = np.linspace(1,len(new_exhalation),len(new_exhalation))
+exhale_evaluation_points = np.linspace(0,len(new_exhalation),25)
+final_exhale = np.interp(exhale_evaluation_points,x_exhale,new_exhalation)
+
+# Get current date time
+instance_time = datetime.datetime.now()
+# print(instance_time)
